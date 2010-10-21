1e0bac69288bfa6791af50798dce86f41d01de23
Shereef Bishay <shereef@gmail.com>
2010-10-21 00:08:29 -0700

    First engagement graph in place

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,47 @@ class VideosController < ApplicationController
   
   def show
     @logs = @video.logs
+    
+    @h = HighChart.new('graph') do |f|
+        f.chart({:defaultSeriesType=>"spline" , :renderTo => "myRenderArea" , :zoomType=> 'x'})
+        f.title({:text => "Engagement Graph"})
+        f.subtitle({:text => "Click and drag in the plot area to zoom in"})
+        f.x_axis(:type=>'datetime', :maxZoom => @video.duration, :title => {:text => "Time"})
+        f.y_axis(:title => {:text => "Engagement"}, :min => 0.6, :startOnTick => false, :showFirstLabel => false)
+        f.legend(:enabled => false)
+        f.plotOptions( :area => {
+                                  :fillColor => {
+                                                 :linearGradient => [0, 0, 0, 300],
+                                                 :stops => [
+                                                    [0, 'rgba(2,0,0,0)'],
+                                                    [1, 'rgba(2,0,0,0)']
+                                                 ]
+                                                 },
+                                  :lineWidth => 1,
+                                  :marker => {
+                                                 :enabled => false,
+                                                 :states => {
+                                                            :hover => {
+                                                                      :enabled => true,
+                                                                      :radius => 5
+                                                                      }
+                                                           }
+                                              },
+                                    :shadow => false,
+                                    :states => {
+                                                 :hover => {
+                                                              :lineWidth => 1                  
+                                                            }
+                                                }
+                                  }
+                      )
+          f.series(:name=>'Engagement', 
+                    :type=>'area',
+                    :pointInterval=> 1000,
+                    :data=> @video.heatmap_array )
+            
+                      
+      end
   end
   
   private
